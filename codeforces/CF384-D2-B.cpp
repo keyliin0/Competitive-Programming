9dce55a2dd8db28e5631d8843a66b6e7f560285f
keyliin0 <35576004+keyliin0@users.noreply.github.com>
2018-07-31 09:48:15 +0100

    Add files via upload

---
+++ codeforces/CF384-D2-B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,b,e) for(int i=b;i<=e;i++)
+#define loop2(i,e,b) for(int i=e;i>=b;i--)
+#define io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define X real()
+#define Y imag()
+#define cp(a,b)	( (conj(a)*(b)).imag() )
+#define length(a) (hypot((a).imag(), (a).real()))
+typedef complex<double> point;
+typedef long long ll;
+typedef unsigned long long ull;
+double PI = 3.141592653589793;
+const double EPS = 1e-9;
+const int N = 1e6 + 5;
+const ll mod = 1e9 + 7;
+const int oo = 1e9;
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+int n, m,k;
+
+int main() {
+	scanf("%d%d%d",&n,&m,&k);
+	printf("%d\n", (m*(m - 1)) / 2);
+	if (k == 0) {
+		loop(i, 1, m)
+			loop(j, i + 1, m)
+			printf("%d %d\n", i, j);
+	}
+	else {
+		loop2(i,m,2)
+			loop2(j,i-1,1)
+			printf("%d %d\n", i, j);
+	}
+}
