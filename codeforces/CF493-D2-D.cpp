51aa21355c0a0013c26646effa854cafa899b400
keyliin0 <35576004+keyliin0@users.noreply.github.com>
2018-08-20 12:00:35 +0100

    Add files via upload

---
+++ codeforces/CF493-D2-D.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,b,e) for(int i=b;i<=e;i++)
+#define loop2(i,e,b) for(int i=e;i>=b;i--)
+#define io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define X real()
+#define Y imag()
+#define cp(a,b)    ( (conj(a)*(b)).imag() )
+#define length(a) (hypot((a).imag(), (a).real()))
+typedef complex<double> point;
+typedef long long ll;
+typedef unsigned long long ull;
+double PI = 3.141592653589793;
+const double EPS = 1e-9;
+const int N = 1e6 + 5;
+const ll mod = 1e9 + 7;
+const int oo = 1e9;
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+int n;
+
+int main() {
+	scanf("%d",&n);
+	if (n % 2) {
+		printf("black");
+	}
+	else {
+		printf("white\n1 2");
+	}
+}
