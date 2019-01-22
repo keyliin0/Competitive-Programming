d124aa9facde51f216c16d36b5500791c600248f
keyliin0 <35576004+keyliin0@users.noreply.github.com>
2019-01-22 23:26:18 +0100

    Add files via upload

---
+++ codeforces/CF1104-D2-B.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,b,e) for(int i=b;i<=e;i++)
+#define loop2(i,e,b) for(int i=e;i>=b;i--)
+#define io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define X real()
+#define Y imag()
+#define cp(a,b)    ( (conj(a)*(b)).imag() )
+#define length(a) (hypot((a).imag(), (a).real()))
+typedef complex<double> point;
+typedef long long ll;
+typedef unsigned long long ull;
+double PI = 3.141592653589793;
+const double EPS = 1e-8;
+const int N = 1e6 + 5;
+const ll mod = 1e9 + 7;
+const int oo = 1e9;
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+string s;
+
+int main() {
+	cin >> s;
+	stack<char> st;
+	int cnt = 0;
+	loop(i, 0, s.size() - 1) {
+		if (!st.empty() && st.top() == s[i]) {
+			cnt++;
+			st.pop();
+		}
+		else st.push(s[i]);
+	}
+	cout << (cnt % 2 == 0 ? "No" : "Yes");
+}
