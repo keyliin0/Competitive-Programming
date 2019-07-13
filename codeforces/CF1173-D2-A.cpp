5d2c09c8ee84addbf872a9fc952465d6e5c54fee
keyliin0 <35576004+keyliin0@users.noreply.github.com>
2019-07-13 22:40:57 +0100

    Add files via upload

---
+++ codeforces/CF1173-D2-A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i,b,e) for(int i=b;i<=e;i++)
+#define loop2(i,e,b) for(int i=e;i>=b;i--)
+#define io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define X real()
+#define Y imag()
+#define cp(a,b)    ( (conj(a)*(b)).imag() )
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define length(a) (hypot((a).imag(), (a).real()))
+#define rotateO(p,ang) ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define logg2  0.30102999566398119521373889472449L
+typedef complex<double> point;
+typedef long long ll;
+typedef unsigned long long ull;
+double PI = 3.141592653589793;
+const double EPS = 1e-6;
+const int N = 1e6 + 5;
+const ll mod = 1e9 + 7;
+const int oo = 1e9;
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+ 
+int x, y, z;
+ 
+int main() {
+	cin >> x >> y >> z;
+	char ans;
+	int w = x - y;
+	if (w == 0 && z || z &&  w + z >= 0 && w - z <= 0) ans = '?';
+	else if (w == 0) ans = '0';
+	else if (w > 0) ans = '+';
+	else ans = '-';
+	cout << ans;
+}
