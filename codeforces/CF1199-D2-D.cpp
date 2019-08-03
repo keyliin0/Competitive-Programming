fca92cd749ad2818688fd9350d1e40b01f0d7cb3
keyliin0 <35576004+keyliin0@users.noreply.github.com>
2019-08-03 21:38:21 +0100

    Add files via upload

---
+++ codeforces/CF1199-D2-D.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i,b,e) for(int i=b;i<=e;i++)
+#define loop2(i,e,b) for(int i=e;i>=b;i--)
+#define io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define X real()
+#define Y imag()
+#define cp(a,b)    ( (conj(a)*(b)).imag() )
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define length(a) (hypot((a).imag(), (a).real()))
+#define rotateO(p,ang) ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define logg2  0.30102999566398119521373889472449L
+typedef complex<double> point;
+typedef long long ll;
+typedef unsigned long long ull;
+double PI = 3.141592653589793;
+const double EPS = 1e-6;
+const int N = 1e6 + 5;
+const ll mod = 1e9 + 7;
+int oo = 1e9;
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+ 
+int n;
+int t[N];
+int vis[N];
+int q;
+ 
+int main() {
+	scanf("%d",&n);
+	loop(i, 1, n)
+		scanf("%d", &t[i]);
+	vector<int> suf;
+	suf.push_back(0);
+	scanf("%d",&q);
+	loop(i,1,q) {
+		int type, p, x;
+		scanf("%d",&type);
+		if (type == 1) {
+			scanf("%d%d", &p, &x);
+			vis[p] = i;
+			t[p] = x;
+			suf.push_back(0);
+		}
+		else {
+			scanf("%d",&x);
+			suf.push_back(x);
+		}
+	}
+	suf.push_back(0);
+	loop2(i, q - 1, 0) {
+		suf[i] = max(suf[i], suf[i + 1]);
+	}
+	loop(i, 1, n) {
+		t[i] = max(t[i], suf[vis[i] + 1]);
+		printf("%d ", t[i]);
+	}
+}
