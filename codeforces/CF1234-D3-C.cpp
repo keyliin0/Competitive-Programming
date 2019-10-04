92bd418b9f1f1b0884b5a0f9de75ac4a0149d635
keyliin0 <35576004+keyliin0@users.noreply.github.com>
2019-10-04 20:11:24 +0100

    Add files via upload

---
+++ codeforces/CF1234-D3-C.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i,b,e) for(int i=b;i<=e;i++)
+#define loop2(i,e,b) for(int i=e;i>=b;i--)
+#define io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define X real()
+#define Y imag()
+#define cp(a,b)    ( (conj(a)*(b)).imag() )
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define length(a) (hypot((a).imag(), (a).real()))
+#define rotateO(p,ang) ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define logg2  0.30102999566398119521373889472449L
+typedef complex<double> point;
+typedef long long ll;
+typedef unsigned long long ull;
+double PI = 3.141592653589793;
+const double EPS = 1e-6;
+const int N = 1e6 + 5;
+const ll mod = 1e9 + 7;
+int oo = 1e9;
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+int n, q;
+char s[2][200005];
+
+int main() {
+	scanf("%d", &q);
+	while (q--) {
+		scanf("%d", &n);
+		scanf("%s%s", s[0], s[1]);
+		int x = 0, y = 0;
+		while (x < n) {
+			if (s[y][x] == '2' || s[y][x] == '1') x++;
+			else if (s[!y][x] >= '3') x++, y = !y;
+			else break;
+		}
+		if (x == n && y == 1) cout << "YES\n";
+		else cout << "NO\n";
+	}
+}
