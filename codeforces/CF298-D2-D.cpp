02d34ca62e3710e57704b7bacce7dfc84ed49a39
keyliin0 <35576004+keyliin0@users.noreply.github.com>
2019-10-21 21:45:19 +0100

    Add files via upload

---
+++ codeforces/CF298-D2-D.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i,b,e) for(int i=b;i<=e;i++)
+#define loop2(i,e,b) for(int i=e;i>=b;i--)
+#define io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define fi first
+#define se second
+#define X real()
+#define Y imag()
+#define cp(a,b)    ( (conj(a)*(b)).imag() )
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define length(a) (hypot((a).imag(), (a).real()))
+#define rotateO(p,ang) ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define logg2  0.30102999566398119521373889472449L
+typedef complex<double> point;
+typedef long long ll;
+typedef unsigned long long ull;
+double PI = 3.141592653589793;
+const double EPS = 1e-6;
+const int N = 1e6 + 5;
+const ll mod = 1e9 + 7;
+int oo = 1e9;
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+int n, m, k;
+map<int, int> mp;
+
+int a[N], b[N];
+
+int main() {
+	scanf("%d%d%d", &n, &m, &k);
+	loop(i, 0, n - 1)
+		scanf("%d", &a[i]);
+	loop(i, 0, m - 1)
+		scanf("%d", &b[i]);
+	sort(a, a + n);
+	reverse(a, a + n);
+	sort(b, b + m);
+	reverse(b, b + m);
+	loop(i, 0, min(n, m) - 1) {
+		if (a[i] <= b[i]) continue;
+		puts("YES");
+		return 0;
+	}
+	if(n > m) puts("YES");
+	else puts("NO");
+}
